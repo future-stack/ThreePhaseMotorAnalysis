f36076c91eb279961f8806b6ab4c67b49857344f
Kevin Matheus Martins <kevin.martins@dextra-sw.com>
2019-11-20 23:06:16 -0300

    restrict fields to search

--- src/domain/motorAnalyseDomain.js
+++ src/domain/motorAnalyseDomain.js
@@ -2,7 +2,7 @@ import commonDBConnection from '../infra/commonDBConnection';
 
 export const findAllData = async () => {
     try {
-        const data = await commonDBConnection.select('*').from('motor_data');
+        const data = await commonDBConnection.select(['electric_current_a','electric_current_b','electric_current_c','read_data']).from('motor_data');
         return data;
     } catch (e) {
         console.log('error', e);
